8cf080586eeff6a2a8ea4bd8b12a21959642598e
Mr-Ndi <onlythenotes@gmail.com>
2025-12-02 06:50:27 +0200

    Middleware/Validation schema in the house

---
+++ BackEnd/src/middleware/validation.py
@@ -0,0 +1,35 @@
+from uuid import UUID
+from pydantic import BaseModel, ConfigDict, Field
+from typing import Optional
+from datetime import date
+from src.model.Usermodel import UserRole
+
+# Input schema - used for registration (does not include id)
+class userCreateSchema(BaseModel):
+    email: str = Field(..., example="tester@gmail.com")
+    password: str = Field(..., example="Tester#250")
+    full_name: str = Field(..., example="First Tester")
+    role: UserRole = UserRole.USER
+    model_config = ConfigDict(from_attributes=True)
+
+    
+# Output schema - used for the response (includes id)
+class UserBase(BaseModel):
+    id: UUID = Field(exclude=True)
+    email: str
+    full_name: Optional[str] = None
+    role: UserRole = UserRole.USER
+    is_verified: bool = False
+    model_config = ConfigDict(from_attributes=True)
+
+
+class UserUpdateSchema(BaseModel):
+    email: Optional[str] = Field(None, example = "tester@gmail.com")
+    password: Optional[str] =Field( None, example = "Tester#250")
+    full_name: Optional[str] =Field( None, example = "First Tester")
+    role: Optional[UserRole] = None
+
+
+class UserLoginSchema(BaseModel):
+    email: str = Field(..., example = "tester@gmail.com")
+    password: str = Field(..., example = "Tester#250")
