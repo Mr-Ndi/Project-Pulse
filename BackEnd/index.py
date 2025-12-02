4af1ccd562af575ed1e268620f181ef344018c9a
Mr-Ndi <onlythenotes@gmail.com>
2025-12-02 06:03:20 +0200

    Fix: health live server

--- BackEnd/index.py
+++ BackEnd/index.py
@@ -1,6 +1,12 @@
-from fastApi import FASTAPI
+from fastapi import FastAPI
+import uvicorn
 
-app = FASTAPI()
-@app.get("")
+app = FastAPI()
+
+@app.get("/")
 async def health():
     return {"message": "Satisied ?"}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=5000)
